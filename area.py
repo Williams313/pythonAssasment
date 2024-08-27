ba67e4fc650e25352699759dca6681e8a5cf5654
Williams313 <williamslambert313@gmail.com>
2024-08-27 11:24:27 -0700

    commit

---
+++ area.py
@@ -0,0 +1,21 @@
+
+
+
+'''
+make  radius to be  10
+pie to be 3.142
+ calculate pie * radius*radius
+area will be equal to radius*radius*
+  print
+'''
+
+
+ radius = 10;
+         pie = 3.142
+
+        area = 3.142 * radius * radius
+        print("area: ", (area));
+
+
+   
+     	 	
---
+++ bytemultiplication.py
@@ -0,0 +1,17 @@
+
+"""
+prompt user to enter password
+"""
+
+
+
+
+
+
+
+pasword =input("enter your pasword") 
+
+ pasword_counter = len(password)"*"
+    
+
+print(password)
